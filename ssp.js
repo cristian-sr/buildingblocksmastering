fbb447c6b37c47b93554cf8a5d61a29a4953f220
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-17 19:04:09 -0600

    37 done

--- ssp.js
+++ ssp.js
@@ -517,7 +517,10 @@ function cube(num){
   addArrayProperty(myObj, 'myProperty', myArray);
   console.log(myObj.myProperty); // --> [1, 3]*/
   
-  
+  function addArrayProperty(obj, key, arr){
+      obj[key] = arr;
+      return arr[key];
+  }
   
   /*preImmersive-buildingBlocksMastery-038-getNthElement
   Write a function called “getNthElement”.
