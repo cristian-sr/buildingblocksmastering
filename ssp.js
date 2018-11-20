cd779478b296fe26f0e37eb33747b91c28b982af
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-20 03:05:54 -0600

    107 done

--- ssp.js
+++ ssp.js
@@ -1953,6 +1953,19 @@ function cube(num){
   var output = getLengthOfLongestElement(['one', 'two', 'three']);
   console.log(output); // --> 5*/
   
+  function getLengthOfLongestElement(arr){
+
+    var longest = arr[0];
+    if (arr.length < 1){
+        return 0;
+    } 
+    for (var i=0; i < arr.length; i++){
+        if (arr[i].length > longest.length){
+            longest = arr[i]''
+        }
+    }
+    return longest.length;
+  }
   
   
   /*preImmersive-buildingBlocksMastery-108-getSmallestElementAtProperty*
