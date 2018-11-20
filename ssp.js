083cc1ab30a3f2d44dbcc906d4e71577ac13b50c
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-20 00:45:21 -0600

    66 done

--- ssp.js
+++ ssp.js
@@ -993,7 +993,17 @@ function cube(num){
   var output = or(true, false);
   console.log(output); // --> true; */
   
-  
+  function or(expression1, expression2){
+      if (expression1 === true && expression2 === true){
+          return true;
+      } else if (expression1 === true && expression2 === false){
+          return true;
+      } else if (expression1 === false && expression2 === true){
+          return true;
+      } else {
+          return false;
+      }
+  }
   
   
   /*preImmersive-buildingBlocksMastery-067-isEitherEvenOrAreBoth7
