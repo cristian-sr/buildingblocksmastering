e7ce4a4807de8b7630d4e1bafa3908ff9b874894
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-20 04:42:50 -0600

    133 done

--- ssp.js
+++ ssp.js
@@ -2678,7 +2678,6 @@ function getElementOfArrayProperty(obj, key , n){
   }
 
   
-  
   /*preImmersive-buildingBlocksMastery-133-convertScoreToGrade
   Write a function called “convertScoreToGrade”.
   
@@ -2695,7 +2694,22 @@ function getElementOfArrayProperty(obj, key , n){
   var output = convertScoreToGrade(91);
   console.log(output); // --> 'A'*/
   
-  
+  function convertScoreToGrade(score){
+    if (score >= 90 && score <= 100){
+        return 'A';
+      }else if (score >= 80 && score <=89){
+        return 'B';
+      }else if (score >=70 && score <=79){
+        return 'C';
+      }else if (score >= 60 && score <=69){
+        return 'D';
+      }else if (score <=59 && score>=0){
+        return 'F';
+      }else{
+        return 'INVALID SCORE';
+    }
+      
+  }
   
   
   /*preImmersive-buildingBlocksMastery-134-convertScoreToGradeWithPlus
