0b429657c971cfbf45d5256ffc1085914d591652
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-20 03:55:46 -0600

    118 done

--- ssp.js
+++ ssp.js
@@ -2240,6 +2240,20 @@ function getElementOfArrayProperty(obj, key , n){
   var output = findSmallestElement([4, 1, 9, 10]);
   console.log(output); // --> 1*/
   
+  const findSmallestElement = arr => {
+      let smallest = arr[0];
+
+      if (arr.length < 1){
+          return 0;
+      }
+
+      for (let i=0; i<arr.length; i++){
+          if (arr[i] < smallest) {
+              smallest =  arr[i];
+          }
+      }
+    return smallest;
+  }
   
   
   /*preImmersive-buildingBlocksMastery-119-findShortestElement*
