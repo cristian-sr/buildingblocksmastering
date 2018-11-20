d53ba51ce61b1552a1b23df647942793bc2babe9
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-20 00:18:45 -0600

    52 done

--- ssp.js
+++ ssp.js
@@ -724,7 +724,9 @@ function cube(num){
   var output = getLengthOfThreeWords('some', 'other', 'words');
   console.log(output); // --> 14 */
   
-  
+  function getLengthOfThreeWords(word1, word2, word3){
+      return (word1.length + word2.length + word3.length)
+  }
   
   /*preImmersive-buildingBlocksMastery-053-joinArrays
   Write a function called “joinArrays”.
